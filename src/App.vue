5c7a8ad8b0ddc384dbd0bffdd639ec6f8490820b
Alexander Guevara Benites <wguevarab7@gmail.com>
2017-10-15 23:36:06 -0500

    pendiente de terminar

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <!-- use the modal component, pass in the prop -->
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">custom header</h3>
+    </modal>
+  </div>
+</template>
+
+<script>
+import modal from './components/cModal.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      showModal: false,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    modal
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
